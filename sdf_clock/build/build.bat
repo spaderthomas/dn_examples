23fe0449fd81db60ea4b9c15dd2041927f3b1437
spaderthomas <thomas.spader@gmail.com>
2024-12-01 21:32:06 -0500

    build script update, dn

--- sdf_clock/build/build.bat
+++ sdf_clock/build/build.bat
@@ -1,8 +1,11 @@
+@echo off
 set project=SdfClock
 
+@echo on
 mkdir intermediate
 copy ..\..\thirdparty\doublenickel\external\lib\debug\steam_api64.dll .
 
+@echo on
 cl.exe ^
 /Zi /Od /JMC ^
 /Fe%project%.exe /Fo.\intermediate\ /Fd.\intermediate\ ^
@@ -16,3 +19,13 @@ cl.exe ^
 /D "FM_DEBUG" /D "_CRT_SECURE_NO_WARNINGS" /D "_SILENCE_CXX17_ALL_DEPRECATION_WARNINGS" ^
 /link /LIBPATH:"../../thirdparty/doublenickel/external/lib/debug" /DEBUG:FULL /MACHINE:X64 /NOLOGO /SUBSYSTEM:CONSOLE /INCREMENTAL:NO /NOIMPLIB /NOEXP /PDB:.\intermediate\ ^
 "freetype-2.10.4-windows-x64.lib" "glfw-3.3.8-windows-x64.lib" "luajit-2.1.0.3-windows-x64.lib" "steam_api64.lib" "user32.lib" "opengl32.lib" "gdi32.lib" "Shell32.lib" "Kernel32.lib" "Advapi32.lib" "Ole32.lib" "OleAut32.lib" 
+
+@echo off
+if %errorlevel% neq 0 (
+    pause
+    exit
+)
+
+.\%project%.exe
+pause
+exit
